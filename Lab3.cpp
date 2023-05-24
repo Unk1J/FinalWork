8810dc83b1969ead8962010f751ed3f2707eb364
Alexey <alex@yandex.ru>
2023-05-24 22:09:17 +0300

    Добавил файл проекта

---
+++ Lab3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+
+	setlocale(LC_ALL, "Russian");
+	int x;
+	printf("Введите число");
+	scanf_s("%d", &x);
+	if (x< -5 || x>-1)
+	{
+		printf("Число подходит");
+	}
+	else
+	{
+		printf("Число не подходит");
+	}
+}
+
